bb8ebb99b1474c4d60c714d750d1a0cc3f4d8a86
FelipeBarroso <felipebarroso299@gmail.com>
2022-04-01 13:26:26 -0300

    Ajuste nos comentarios

--- IMC/imc.js
+++ IMC/imc.js
@@ -1,34 +1,41 @@
 const calculate = document.getElementById('calculate')
 
 function imc() {
+
+    //criar constantes com os elementos do html
     const name = document.getElementById('name').value;
     const height = document.getElementById('height').value;
     const weight = document.getElementById('weight').value;
     const result = document.getElementById('result')
+    
 
-if (name !== '' && height !=='' && weight !== '') {
-    //Aqui vamos o calculo do IMC
-    const valueImc = (weight/(height * height)).toFixed(1);
-    //Aqui vamos criar uma classificação para colocar no grau definido pelo calculo
-    let classification = '';
-
-
-    if (valueImc < 18.5) {
-        classification = 'abaixo do peso.'
+    //Criar as condições
+    if (name !== '' && height !=='' && weight !== '') {
+        //Aqui vamos o calculo do IMC
+        const valueImc = (weight/(height * height)).toFixed(1);
+        //Aqui vamos criar uma classificação para colocar no grau definido pelo calculo
+        let classification = 'está com obesidade grau 3';
+       //condições
+        if (valueImc < 18.5) {
+            classification = 'está abaixo do peso.'
     } else if (valueImc < 25) {
-        classification = 'com o peso ideal'
+        classification = 'está com o peso ideal'
     } else if (valueImc < 30) {
         classification = 'levemente acima do peso'
     } else if (valueImc < 35) {
-        classification = 'com obesidade grau 1'
+        classification = 'está com obesidade grau 1'
     } else if (valueImc < 40) {
-        classification = 'com obesidade grau 2'
+        classification = 'está com obesidade grau 2'
     } else {
-        classification = 'com obesisdade grau 3'
+        classification = 'está com obesisdade grau 3'
     }
+        //criar a caixa de resultado que irá enviar os dados preenchidos
     result.textContent =`${name} seu IMC é ${valueImc} e você ${classification}`;
-    } else {
+    } 
+        //Caso não esteja preenchido
+        else {
         result.textContent = 'Preencha os campos!!!';
     }
 }
+//chama a função
 calculate.addEventListener('click', imc)
